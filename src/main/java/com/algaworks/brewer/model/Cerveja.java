f576c605f5e35abd412b4fa5baf1e225729b5140
victor-carvalho1996 <victorpcarvalho18@gmail.com>
2022-03-03 22:06:41 -0300

    Commit Parcial

--- src/main/java/com/algaworks/brewer/model/Cerveja.java
+++ src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -1,19 +1,77 @@
 package com.algaworks.brewer.model;
 
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.NotBlank;
 
+import com.algaworks.brewer.validation.SKU;
+
+@Entity
+@Table(name = "cerveja")
 public class Cerveja {
 
-	@NotBlank(message = "SKU e um campo obrigatório.")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+
+	@SKU
+	@NotBlank(message = "SKU é obrigatório")
 	private String sku;
-	
-	@NotBlank(message = "NOME e um campo obrigatório.")
+
+	@NotBlank(message = "Nome é obrigatório")
 	private String nome;
-	
-	@Size(min = 1, max = 50, message = "O tamanho da descrição deve estar entre 1 e 50 caracteres.")
+
+	@NotBlank(message = "A descrição é obrigatória")
+	@Size(max = 50, message = "O tamanho da descrição deve estar entre 1 e 50")
 	private String descricao;
+
+	@NotNull(message = "Valor é obrigatório.")
+	@DecimalMin("0.01")
+	@DecimalMax(value = "9999999.99", message = "O valor da cerveja deve ser menor que R$9.999.999,99")
+	private BigDecimal valor;
+
+	@NotNull(message = "O teor alcóolico é obrigatório.")
+	@DecimalMax(value = "100.0", message = "O valor do teor alcóolico deve ser menor que 100")
+	@Column(name = "teor_alcoolico")
+	private BigDecimal teorAlcoolico;
+
+	@NotNull(message = "A Comissão é obrigatória")
+	@DecimalMax(value = "100.0", message = "A comissão deve ser igual ou menor que 100")
+	private BigDecimal comissao;
+
+	@NotNull(message = "O estoque é obrigatório")
+	@Max(value = 9999, message = "A quantidade em estoque deve ser menor que 9.999")
+	@Column(name = "quantidade_estoque")
+	private Integer quantidadeEstoque;
+
+	@NotNull(message = "A origem é obigatória")
+	@Enumerated(EnumType.STRING)
+	private Origem origem;
+
+	@NotNull(message = "A sabor é obigatória")
+	@Enumerated(EnumType.STRING)
+	private Sabor sabor;
+
+	@NotNull(message = "A estilo é obigatória")
+	@ManyToOne
+	@JoinColumn(name = "codigo_estilo")
+	private Estilo estilo;
 
 	public String getSku() {
 		return sku;
@@ -30,7 +88,7 @@ public class Cerveja {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
 	public String getDescricao() {
 		return descricao;
 	}
@@ -38,10 +96,94 @@ public class Cerveja {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-	
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public BigDecimal getTeorAlcoolico() {
+		return teorAlcoolico;
+	}
+
+	public void setTeorAlcoolico(BigDecimal teorAlcoolico) {
+		this.teorAlcoolico = teorAlcoolico;
+	}
+
+	public BigDecimal getComissao() {
+		return comissao;
+	}
+
+	public void setComissao(BigDecimal comissao) {
+		this.comissao = comissao;
+	}
+
+	public Integer getQuantidadeEstoque() {
+		return quantidadeEstoque;
+	}
+
+	public void setQuantidadeEstoque(Integer quantidadeEstoque) {
+		this.quantidadeEstoque = quantidadeEstoque;
+	}
+
+	public Origem getOrigem() {
+		return origem;
+	}
+
+	public void setOrigem(Origem origem) {
+		this.origem = origem;
+	}
+
+	public Sabor getSabor() {
+		return sabor;
+	}
+
+	public void setSabor(Sabor sabor) {
+		this.sabor = sabor;
+	}
+
+	public Estilo getEstilo() {
+		return estilo;
+	}
+
+	public void setEstilo(Estilo estilo) {
+		this.estilo = estilo;
+	}
+
 	@Override
-	public String toString() {
-		return "sku: " + getSku() + " nome: " + getNome();
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cerveja other = (Cerveja) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
 	}
 
 }
--- src/main/java/com/algaworks/brewer/model/Estilo.java
+++ src/main/java/com/algaworks/brewer/model/Estilo.java
@@ -1,16 +1,74 @@
 package com.algaworks.brewer.model;
 
-public class Estilo {
+import java.io.Serializable;
+import java.util.List;
 
-	private String nomeEstilo;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
 
-	public String getNomeEstilo() {
-		return nomeEstilo;
-	}
+import org.hibernate.validator.constraints.NotBlank;
 
-	public void setNomeEstilo(String nomeEstilo) {
-		this.nomeEstilo = nomeEstilo;
-	}
+@Entity
+@Table(name = "estilo")
+public class Estilo implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+
+	@NotBlank(message =  "O nome do estilo é obrigatório")
+	@Size(max = 20, message = "O tamanho do nome não pode ser maior que {max} caracteres")
+	private String nome;
 	
+	@OneToMany(mappedBy = "estilo")
+	private List<Cerveja> cervejas;
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Estilo other = (Estilo) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+
 }
